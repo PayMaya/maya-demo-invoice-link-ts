4453f99ea17a74cb6f3bfc70fa0adb6d5521d270
millany.cua <millany.cua@paymaya.com>
2023-03-01 15:13:31 +0800

    updated config variables

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
     host_url: process.env.REACT_APP_HOST_URL || 'http://localhost:3000',
     path_prefix: process.env.REACT_APP_PATH_PREFIX || '/',
-    maya_payments: {
-      url: 'https://pg-sandbox.paymaya.com/payments/v1',
+    invoice: {
+      url: 'https://pg-sandbox.paymaya.com/invoice/v2',
       pub_api_key: process.env.REACT_APP_VAULT_PUBLIC_API_KEY,
       sec_api_key: process.env.REACT_APP_VAULT_SECRET_API_KEY,
     },
