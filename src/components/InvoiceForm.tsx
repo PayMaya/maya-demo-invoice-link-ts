241ca9b4faa59837b89c131db453bcd83f051a6a
millany.cua <millany.cua@paymaya.com>
2023-03-02 15:50:03 +0800

    added invoice items form | added types

---
+++ src/components/InvoiceForm.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+
+interface InvoiceItem {
+    name: string
+    quantity: number
+    unitPrice: number
+    amount: number
+}
+
+function InvoiceForm() {
+    const initItem: InvoiceItem = {
+        name: '',
+        quantity: 0,
+        unitPrice: 0,
+        amount: 0
+    }
+    const sampleItem: InvoiceItem = {
+        name: 'Product 1',
+        quantity: 2,
+        unitPrice: 100,
+        amount: 200
+    }
+    const [items, setItems] = useState([sampleItem])
+
+    const addNewItem = () => {
+        setItems([ ...items, initItem ])
+    }
+
+    const setItemValue = (e: React.ChangeEvent<HTMLInputElement>, index: number, field: string) => {
+        const value: string | number = isNaN(+(e.target.value)) ? e.target.value : e.target.value
+        const updatedItem = { ...items[index], [field]: value }
+        updatedItem.amount = updatedItem.quantity * updatedItem.unitPrice
+        const updatedItems = items.slice()
+        updatedItems[index] = updatedItem
+        setItems(updatedItems)
+    }
+
+    const deleteItem = (e: React.SyntheticEvent, index: number) => {
+        e.preventDefault()
+        const updatedItems = items.slice()
+        console.log(updatedItems[index])
+        updatedItems.splice(index, 1)
+        setItems(updatedItems)
+    }
+
+    return (
+        <>  
+            <div className="invoice-form-layout">
+                <p className="long-label"> Name </p>
+                <p> Quantity </p>
+                <p> Unit Price </p>
+                <p> Amount </p>
+            </div>
+                {items.map((item, index) => {
+                    return (
+                        <div key={index} className="invoice-form-layout">
+                            <button className="btn red" onClick={(e: React.SyntheticEvent) => deleteItem(e, index)}> X </button>
+                            <input 
+                                type="text"
+                                placeholder="Item Name" 
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setItemValue(e, index, 'name')}
+                                value={item.name}
+                                required 
+                            />
+                            <input 
+                                type="number" 
+                                className="short-input"
+                                placeholder="Quantity" 
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setItemValue(e, index, 'quantity')}
+                                value={+item.quantity} 
+                                min={0}
+                                required
+                            />
+                            <input 
+                                type="number"
+                                className="short-input"
+                                placeholder="Unit Price"
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setItemValue(e, index, 'unitPrice')} 
+                                value={+item.unitPrice}
+                                min={0}
+                                required
+                            />
+                            <input 
+                                type="number" 
+                                className="short-input" 
+                                placeholder="Amount" 
+                                value={+item.amount} 
+                                disabled 
+                            />
+                        </div>
+                    )
+                })}
+                <div className="total-amount-container">
+                    <label> Total Amount: {items.map(item => item.amount).reduce((a, b) => a + b, 0)} </label>
+                </div>
+            
+            <div className="add-new-item-container">
+                <button className="btn green" onClick={() => addNewItem()}>Add New Item</button>
+            </div>
+        </>
+    )
+}
+
+export default InvoiceForm
--- src/views/InvoicePage.tsx
+++ src/views/InvoicePage.tsx
@@ -1,6 +1,7 @@
 import ContentSection from "../components/ContentSection";
 import Footer from "../components/Footer";
 import HomeButton from "../components/HomeButton";
+import InvoiceForm from "../components/InvoiceForm";
 import NavBar from "../components/NavBar";
 import TitleSection from "../components/TitleSection";
 import UserCard from "../components/UserCard";
@@ -14,7 +15,12 @@ function InvoicePage() {
             />
             <TitleSection text='Invoice'/>
             <ContentSection>
-                <p> InvoicePage.tsx </p>
+                    <form name="form1" className="invoice-form">
+                        <div className="form-container">
+                            <h2> Items </h2>
+                            <InvoiceForm />
+                        </div>
+                    </form>
             </ContentSection>
             <Footer />
         </div>
