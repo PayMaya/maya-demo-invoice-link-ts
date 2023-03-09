bc581bc26ba08160d92316f22dd221fdee207d35
millany.cua <millany.cua@paymaya.com>
2023-03-09 14:58:11 +0800

    changed form layouts

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -2,25 +2,27 @@ function TransactionForm() {
     return (
         <>  
             <div className="transaction-form-layout">
-                <div className="container">
-                    <label className="item"> Invoice Number </label> <br />
-                    <input 
-                        type="text"
-                        name="invoiceNum"
-                        className="item"
-                        placeholder="Invoice Number"
-                        required 
-                    />
-                </div>  
-                <div className="container">
-                    <label className="item"> Request Reference Number </label> <br />
-                    <input 
-                        type="text"
-                        name="rrn"
-                        className="item"
-                        placeholder="Request Reference Number"
-                        required 
-                    />
+                <div className="input-row-container">
+                    <div className="input-container">
+                        <label className="item"> Invoice Number </label> <br />
+                        <input 
+                            type="text"
+                            name="invoiceNum"
+                            className="item"
+                            placeholder="Invoice Number"
+                            required 
+                        />
+                    </div>
+                    <div className="input-container">
+                        <label className="item"> Request Reference Number </label> <br />
+                        <input 
+                            type="text"
+                            name="rrn"
+                            className="item"
+                            placeholder="Request Reference Number"
+                            required 
+                        />
+                    </div>
                 </div>
             </div>
         </>
